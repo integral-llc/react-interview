eb4bffbab862616638ffe45076b6c9eae8110bb2
Eugen Rata <eugen.rata@gmail.com>
2016-11-22 10:01:51 -0700

    Fixed the description for `MiniRedux` exercise

--- subjects/MiniRedux/exercise.js
+++ subjects/MiniRedux/exercise.js
@@ -1,31 +1,16 @@
-/*
-The goal of this exercise is to add a button beside each contact in the list
-that can be used to delete that contact. To do this, you'll need to perform
-the following steps:
+ ////////////////////////////////////////////////////////////////////////////////
+ // Exercise
+ //
+ // 1. Implement <Provider> to make the store accessible on context to the rest
+ //    of the components rendered below it
+ // 2. Implement `connect` it should:
+ //   a) Return a function that takes a component
+ //   b) The new function should return a new component that wraps the component
+ //      passed to it
+ //   c) The new component, when rendered, will pass state from
+ //      the store as props to your App component. You'll use the function
+ //      passed to `connect` to map store state to component props
+ //
 
-Remember, redux models all changes to state with an action. Mimic what we've
-done to load the contacts with a new "thunk" action.
 
-The order you do things makes the code easier to iterate: write some code,
-app runs, write some code, app runs. We've found the best workflow with redux
-is to:
-
-1. add a reducer, in this exercise the reducer is already there (reducers/contacts)
-   so this part is already done.
-2. Add propTypes to your component for the state and then "select" it in `connect`
-2. add a new action, it doesn't have to do much yet, just exist
-3. add the UI and dispatch the action
-4. finish up the action's logic if needed
-5. handle the action in the reducer by returning new state, remember don't mutate
-   the reducer state, return new objects/arrays.
-
-Doing it in this order ensures your app will continue to run when you save code.
-
-If you run into problems, check the logger's output:
-
-- Did you dispatch the right action? Check your component code.
-- Did you send the right data in the action? Check your action code.
-- Is the next state what you'd expect? Check the reducer.
-
-*/
 import './exercise/index'
