35efccec7b5264dea1d0e65f512b79b3dbbc0770
Victor <v.urbanas@mosidev.com>
2016-11-22 20:02:54 +0200

    implement higher order compoent MousePosition

--- subjects/HigherOrderComponents/exercise.js
+++ subjects/HigherOrderComponents/exercise.js
@@ -1,16 +1,50 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Exercise:
 //
-// Make `withMousePosition`a a "higher-order component" that sends the mouse
+// Make `withMousePosition` a "higher-order component" that sends the mouse
 // position to the component as props.
 //
 // hint: use `event.clientX` and `event.clientY`
 
 import React from 'react'
-import { render } from 'react-dom'
+import {render} from 'react-dom'
 
 const withMousePosition = (Component) => {
-  return Component
+
+  class MousePosition extends React.Component {
+    state = {
+      x: 0,
+      y: 0
+    };
+
+    constructor(props, context) {
+      super(props, context);
+
+      this.onMouseMove = this.onMouseMove.bind(this);
+    }
+
+    onMouseMove({clientX: x, clientY: y}) {
+      this.setState({x, y});
+    }
+
+
+    componentDidMount() {
+      document.addEventListener('mousemove', this.onMouseMove);
+    }
+
+    componentWillUnmount() {
+      document.removeEventListener('mousemove', this.onMouseMove);
+    }
+
+    render() {
+
+      return <Component mouse={this.state} />;
+    }
+  }
+
+  MousePosition.displayName = `MousePosition${Component.displayName || Component.name || 'Component'}`;
+
+  return MousePosition;
 };
 
 class App extends React.Component {
