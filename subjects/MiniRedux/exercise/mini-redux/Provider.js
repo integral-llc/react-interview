167744ab51c6715c93674404b43e221dd540967f
polzoom <polzoom@mail.ru>
2016-11-23 18:25:26 +0200

    the second task

--- subjects/MiniRedux/exercise/mini-redux/Provider.js
+++ subjects/MiniRedux/exercise/mini-redux/Provider.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 class Provider extends React.Component {
+  getChildContext() {
+    return {store: this.props.store};
+  }
+
   render() {
     return <div>{this.props.children}</div>
   }
 }
+
+Provider.childContextTypes = {
+  store: React.PropTypes.object
+};
+
 
 export default Provider
--- subjects/MiniRedux/exercise/mini-redux/connect.js
+++ subjects/MiniRedux/exercise/mini-redux/connect.js
@@ -2,6 +2,35 @@ import React from 'react'
 
 export default function connect(mapStateToProps) {
   return function (Component) {
-    return Component
+    class WrappedComponent extends React.Component {
+      setCounterState() {
+        this.state = mapStateToProps(this.context.store.getState());
+      }
+
+      componentWillMount() {
+        this.context.store.listen(() => {
+          this.setState(mapStateToProps(this.context.store.getState()));
+        })
+      }
+
+      componentWillUnmount() {
+        this.context.store.removeListener(() => {
+          this.setState(mapStateToProps(this.context.store.getState()));
+      })
+      }
+
+      render() {
+
+        return (
+          <Component {...this.context.store} {...this.state}/>
+        )
+      }
+    }
+
+    WrappedComponent.contextTypes = {
+      store: React.PropTypes.object
+    };
+
+    return WrappedComponent;
   }
 }
